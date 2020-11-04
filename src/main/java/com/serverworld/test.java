dfe1466b4a3f47b18fd47c03eca7e690bb102b22
admin <paul002706@gmail.com>
2020-11-05 00:55:25 +0800

    Hello world

---
+++ src/main/java/com/serverworld/test.java
@@ -0,0 +1,15 @@
+package com.serverworld;
+
+import org.tensorflow.Graph;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+import org.tensorflow.TensorFlow;
+import org.tensorflow.types.TString;
+
+
+public class test {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello world TensorFlow " + TensorFlow.version());
+    }
+}
